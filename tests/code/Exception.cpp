91ecd87c2ce38346dc0a9d2721e3f393ed2785b4
julien.waechter <julien.waechter@gmail.com>
2015-04-29 09:36:23 +0200

    Fixed compilation dcmtkpp tests

--- tests/code/Exception.cpp
+++ tests/code/Exception.cpp
@@ -5,6 +5,7 @@
 
 #include <dcmtk/config/osconfig.h>
 #include <dcmtk/ofstd/ofcond.h>
+#include <dcmtk/dcmdata/dcuid.h>
 
 #include "dcmtkpp/Exception.h"
 
@@ -31,6 +32,10 @@ BOOST_AUTO_TEST_CASE(ConditionConstructor)
 {
     dcmtkpp::Exception const exception(EC_IllegalParameter);
     BOOST_CHECK_EQUAL(exception.get_source(), dcmtkpp::Exception::Source::Condition);
+#if OFFIS_DCMTK_VERSION_NUMBER < 361
     BOOST_CHECK_EQUAL(exception.what(), EC_IllegalParameter.text());
+#else
+    BOOST_CHECK_EQUAL(exception.what(), EC_IllegalParameter.theText);
+#endif
     BOOST_CHECK(exception.get_condition() == EC_IllegalParameter);
 }
